afb289bd4625381ac2f32985845a521f700622e9
Joseph Gabito <codehaiku.io@gmail.com>
2018-06-14 18:58:27 +0800

    update

--- public/js/xchat.js
+++ public/js/xchat.js
@@ -1,7 +1,10 @@
 jQuery(document).ready(function($){
 	var source = new EventSource(__xchat.rest_uri);
 	source.onmessage = function(event) {
-		console.log(event);
-	    console.log(JSON.parse( event.data) );
+		var messages = JSON.parse( event.data);
+		console.log(messages);
+		$.each(messages, function( index, message ){
+			console.log(message);
+		});
 	};
 });
